a22cd329fe96e49a0bf1b3161b8058ca34a4a401
Alastair Martin <alastairmartin@hotmail.com>
2020-08-31 20:50:24 +0100

    commit from laptop

--- GetDateFromYahoo.py
+++ GetDateFromYahoo.py
@@ -8,7 +8,7 @@ returns
 """
 
 # get stock info
-msft.info
+print('*** Info: ',msft.info)
 
 """
 returns:
@@ -24,51 +24,16 @@ returns:
 """
 
 # get historical market data
-print('History',msft.history(period="1wk",interval='5m'))
-"""
-returns:
-              Open    High    Low    Close      Volume  Dividends  Splits
-Date
-1986-03-13    0.06    0.07    0.06    0.07  1031788800        0.0     0.0
-1986-03-14    0.07    0.07    0.07    0.07   308160000        0.0     0.0
-...
-2019-04-15  120.94  121.58  120.57  121.05    15792600        0.0     0.0
-2019-04-16  121.64  121.65  120.10  120.77    14059700        0.0     0.0
-"""
+print('*** History: ',msft.history(period="1wk",interval='5m'))
 
 # show actions (dividends, splits)
-print('Actins',msft.actions)
-"""
-returns:
-            Dividends  Splits
-Date
-1987-09-21       0.00     2.0
-1990-04-16       0.00     2.0
-...
-2018-11-14       0.46     0.0
-2019-02-20       0.46     0.0
-"""
+print('*** Actions: ',msft.actions)
 
 # show dividends
 print('Dividends',msft.dividends)
-"""
-returns:
-Date
-2003-02-19    0.08
-2003-10-15    0.16
-...
-2018-11-14    0.46
-2019-02-20    0.46
-"""
 
 # show splits
 print('Splits',msft.splits)
-"""
-returns:
-Date
-1987-09-21    2.0
-1990-04-16    2.0
-...
-1999-03-29    2.0
-2003-02-18    2.0
-"""
+
+# show quarterly financials
+print(msft.quarterly_financials)

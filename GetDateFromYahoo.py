73bf28051546f9027617c9c02576bb43886e3fb4
Alastair Martin <alastairmartin@hotmail.com>
2020-09-03 22:56:41 +0100

    merged wit home1

--- GetDateFromYahoo.py
+++ GetDateFromYahoo.py
@@ -1,39 +1,78 @@
 import yfinance as yf
 
-msft = yf.Ticker("MSFT")
+msft = yf.Ticker("BA.L")
 print(msft)
-"""
-returns
-<yfinance.Ticker object at 0x1a1715e898>
-"""
 
+print('-----------------------------------------------------------------------------')
 # get stock info
 print('*** Info: ',msft.info)
+for x,y in msft.info.items():
+    print(x,': ',y)
 
-"""
-returns:
-{
- 'quoteType': 'EQUITY',
- 'quoteSourceName': 'Nasdaq Real Time Price',
- 'currency': 'USD',
- 'shortName': 'Microsoft Corporation',
- 'exchangeTimezoneName': 'America/New_York',
-  ...
- 'symbol': 'MSFT'
-}
-"""
-
+print('-----------------------------------------------------------------------------')
 # get historical market data
-print('*** History: ',msft.history(period="1wk",interval='5m'))
+print('*** History: ',msft.history(period="5yr",interval='1h'))
 
+print('-----------------------------------------------------------------------------')
 # show actions (dividends, splits)
 print('*** Actions: ',msft.actions)
 
+print('-----------------------------------------------------------------------------')
 # show dividends
-print('Dividends',msft.dividends)
+print('*** Dividends',msft.dividends)
 
+print('-----------------------------------------------------------------------------')
 # show splits
-print('Splits',msft.splits)
+print('*** Splits',msft.splits)
 
-# show quarterly financials
-print(msft.quarterly_financials)
+print('-----------------------------------------------------------------------------')
+# show financials
+print('*** Financials: ',msft.financials)
+print('*** Quarterly financials: ',msft.quarterly_financials)
+
+print('-----------------------------------------------------------------------------')
+# show major holders
+print('*** Major holders: ',msft.major_holders)
+
+print('-----------------------------------------------------------------------------')
+# show institutional holders
+print('*** Institutional holders: ',msft.institutional_holders)
+
+print('-----------------------------------------------------------------------------')
+# show balance heet
+print('*** Balance sheet: ',msft.balance_sheet)
+print('*** Quarterly balance sheet: ',msft.quarterly_balance_sheet)
+
+print('-----------------------------------------------------------------------------')
+# show cashflow
+print('*** Cashflow: ',msft.cashflow)
+print('*** Quarterly cashflow: ',msft.quarterly_cashflow)
+
+print('-----------------------------------------------------------------------------')
+# show earnings
+print('*** Earnings: ',msft.earnings)
+print('*** Quarterly earnings: ',msft.quarterly_earnings)
+
+print('-----------------------------------------------------------------------------')
+# show sustainability
+print('*** Sustainability: ',msft.sustainability)
+
+print('-----------------------------------------------------------------------------')
+# show analysts recommendations
+print('*** Recommendations: ',msft.recommendations)
+
+print('-----------------------------------------------------------------------------')
+# show next event (earnings, etc)
+print('*** Calendar: ',msft.calendar)
+
+print('-----------------------------------------------------------------------------')
+# show ISIN code - *experimental*
+# ISIN = International Securities Identification Number
+print('*** ISIN: ',msft.isin)
+
+# show options expirations
+#print(msft.options)
+
+# get option chain for specific expiration
+#opt = msft.option_chain('YYYY-MM-DD')
+# data available via: opt.calls, opt.puts
